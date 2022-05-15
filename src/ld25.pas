aee423e7f27e571ba1e9bfeba5d854c308bc49d2
suve <veg@svgames.pl>
2022-05-15 21:05:06 +0200

    Fix game always loading the tutorial save

--- src/ld25.pas
+++ src/ld25.pas
@@ -1,6 +1,6 @@
 (*
  * colorful - simple 2D sideview shooter
- * Copyright (C) 2012-2018 Artur Iwicki
+ * Copyright (C) 2012-2022 suve (a.k.a. Artur Frenszek Iwicki)
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License, version 3,
@@ -685,7 +685,7 @@ Begin
 	end;
 	
 	Write('Loading game... ');
-	Result := LoadGame(GM_TUTORIAL);
+	Result := LoadGame(GM);
 	If(Result) then
 		Writeln('Done.')
 	else
